0c15ced2bedc8500fdfbcf0a0809bc297ec21c45
Weliton Vicente <welitonvicente6@gmail.com>
2026-03-25 06:31:04 -0300

    refactor: remove duplication

--- src/presentation/controllers/SignUp.ts
+++ src/presentation/controllers/SignUp.ts
@@ -19,11 +19,13 @@ export class SignUpController implements Controller {
                 }
             }
 
-            if(httpRequest.body.password !== httpRequest.body.passwordConfirmation){
+            const { email, password, passwordConfirmation } = httpRequest.body
+
+            if (password !== passwordConfirmation) {
                 return badRequest(new InvalidParamError('passwordConfirmation'))
             }
 
-            const isValid = this.emailValidador.isValid(httpRequest.body.email)
+            const isValid = this.emailValidador.isValid(email)
 
             if (!isValid) {
                 return badRequest(new InvalidParamError('email'))
