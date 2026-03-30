ef3ac6505d2762213defab3d058947bb1c362862
Weliton Vicente <welitonvicente6@gmail.com>
2026-03-30 11:51:18 -0300

    test: ensure Bcrypter calls bcrypt with correct values

--- src/enfra/criptography/bcrypt-adapter.spec.ts
+++ src/enfra/criptography/bcrypt-adapter.spec.ts
@@ -2,7 +2,7 @@ import bcrypt from 'bcrypt'
 import { BcrypterAdapter } from './bcrypt-adapter';
 
 describe('Bcrypt Adapter', () => {
-    test('Should call bcrypt eith correct value', async () => {
+    test('Should call bcrypt with correct values', async () => {
         const salt = 12
         const sut = new BcrypterAdapter(salt)
         const hashSpy = jest.spyOn(bcrypt, 'hash')
