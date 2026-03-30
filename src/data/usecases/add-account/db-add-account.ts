109239e0a1950282a6d46d259ac0d159a4443e8f
Weliton Vicente <welitonvicente6@gmail.com>
2026-03-30 11:03:32 -0300

    feat: ensure addAccountRepositry returns an account on sucess

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -3,13 +3,16 @@ export class DbAddAccount implements AddAccount {
 
     private readonly encrypter: Encrypter
     private readonly addAccountRepository: AddAccountRepository
+
     constructor(encrypter: Encrypter, AddAccountRepository: AddAccountRepository) {
         this.encrypter = encrypter
         this.addAccountRepository = AddAccountRepository
     }
+
     async add(accountData: AddAccountModel): Promise<AccountModel> {
         const hashedPassword = await this.encrypter.encrypt(accountData.password)
-        await this.addAccountRepository.add(Object.assign({}, accountData, { password: hashedPassword }))
-        return new Promise(resolve => resolve(null))
+        const account = await this.addAccountRepository.add(Object.assign({}, accountData, { password: hashedPassword }))
+        console.log(account)
+        return account
     }
 }
