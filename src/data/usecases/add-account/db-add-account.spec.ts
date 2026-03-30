5872ba12b20d1a5a4288bee88d6f89ac6eb854f6
Weliton Vicente <welitonvicente6@gmail.com>
2026-03-30 11:03:56 -0300

    test: ensure addAccountRepositry returns an account on sucess

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -110,4 +110,24 @@ describe('DbAddAccount Usecase', () => {
         const promisse = sut.add(accountData)
         await expect(promisse).rejects.toThrow()
     })
+
+
+    test('should return an account if on success', async () => {
+        const { sut } = makeSut();
+
+        const accountData = {
+            name: 'valid_name',
+            email: 'valid_name',
+            password: 'valid_password'
+        }
+
+        const account = await sut.add(accountData)
+        expect(account).toEqual({
+            id: 'valid_id',
+            name: 'valid_name',
+            email: 'valid_name',
+            password: 'valid_password'
+        })
+    })
+
 })
